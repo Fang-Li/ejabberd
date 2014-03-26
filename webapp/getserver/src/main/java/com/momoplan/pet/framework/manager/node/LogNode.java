89625f8996736f9beef795b7ef6e192aaa168dfe
cc14514 <cc14514@github.com>
2014-03-26 15:57:25 +0800

    修正

--- webapp/getserver/src/main/java/com/momoplan/pet/framework/manager/node/LogNode.java
+++ webapp/getserver/src/main/java/com/momoplan/pet/framework/manager/node/LogNode.java
@@ -1,5 +1,7 @@
 package com.momoplan.pet.framework.manager.node;
 
+import java.io.IOException;
+
 import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
@@ -34,36 +36,45 @@ public class LogNode {
 
 	@PostConstruct
 	private void start_link() throws Exception{
-		OtpNode node = new OtpNode(nodeName, nodeCookie);
-		node.ping(nodePing, 5000);
-		OtpMbox box = node.createMbox(nodeBox);
-		logger.debug("start_link...nodeName="+nodeName);
-		logger.debug("start_link...nodeCookie="+nodeCookie);
-		logger.debug("start_link...nodePing="+nodePing);
-		logger.debug("start_link...nodeBox="+nodeBox);
-		while(true){
-			try{
-				//tuple ::> {id,from,to,msgtype,body}
-				OtpErlangObject obj = box.receive();
-				OtpErlangTuple tuple = (OtpErlangTuple)obj;
-				OtpErlangString id = (OtpErlangString)tuple.elementAt(0);
-				OtpErlangString from = (OtpErlangString)tuple.elementAt(1);
-				OtpErlangString to = (OtpErlangString)tuple.elementAt(2);
-				OtpErlangString msgtype = (OtpErlangString)tuple.elementAt(3);
-				OtpErlangBinary bin = (OtpErlangBinary)tuple.elementAt(4);
-				String body = new String(bin.binaryValue());
-				StringBuffer log = new StringBuffer("\02");
-				log.append(id.stringValue()).append("\01");
-				log.append(from.stringValue()).append("\01");
-				log.append(to.stringValue()).append("\01");
-				log.append(msgtype.stringValue()).append("\01");
-				log.append(body);
-				logger.info(log.toString());
-				//TODO write in cassandra
-			}catch(Exception e){
-				logger.error("error", e);
+		new Thread(new Runnable(){
+			@Override
+			public void run() {
+				try {
+					OtpNode node = new OtpNode(nodeName, nodeCookie);
+					node.ping(nodePing, 5000);
+					OtpMbox box = node.createMbox(nodeBox);
+					logger.debug("start_link...nodeName="+nodeName);
+					logger.debug("start_link...nodeCookie="+nodeCookie);
+					logger.debug("start_link...nodePing="+nodePing);
+					logger.debug("start_link...nodeBox="+nodeBox);
+					while(true){
+						try{
+							//tuple ::> {id,from,to,msgtype,body}
+							OtpErlangObject obj = box.receive();
+							OtpErlangTuple tuple = (OtpErlangTuple)obj;
+							OtpErlangString id = (OtpErlangString)tuple.elementAt(0);
+							OtpErlangString from = (OtpErlangString)tuple.elementAt(1);
+							OtpErlangString to = (OtpErlangString)tuple.elementAt(2);
+							OtpErlangString msgtype = (OtpErlangString)tuple.elementAt(3);
+							OtpErlangBinary bin = (OtpErlangBinary)tuple.elementAt(4);
+							String body = new String(bin.binaryValue());
+							StringBuffer log = new StringBuffer("\02");
+							log.append(id.stringValue()).append("\01");
+							log.append(from.stringValue()).append("\01");
+							log.append(to.stringValue()).append("\01");
+							log.append(msgtype.stringValue()).append("\01");
+							log.append(body);
+							logger.info(log.toString());
+							//TODO write in cassandra
+						}catch(Exception e){
+							logger.error("error", e);
+						}
+					}				
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
 			}
-		}
+		}).start();;
 	}
 	
 }
