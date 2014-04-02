bae3dd840933dd73904c611c8d2cdf73ce7e55ec
cc14514 <cc14514@github.com>
2014-04-02 18:27:44 +0800

    aa_hookhandler.erl add counter log

--- v2.1.13/src/aa_hookhandler.erl
+++ v2.1.13/src/aa_hookhandler.erl
@@ -29,7 +29,11 @@
 
 sm_register_connection_hook_handler(SID, JID, Info) -> ok.
 sm_remove_connection_hook_handler(SID, JID, Info) -> ok.
-user_available_hook_handler(JID) -> ok.
+user_available_hook_handler(#jid{server=Domain}=JID) -> 
+	%% 统计并发量
+	Total = aa_session:total_count_user(Domain),		
+	log({counter,Domain,Total}),
+	ok.
 
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -396,6 +400,22 @@ conn_ecache_node() ->
 	end.
 
 %% {id,from,to,msgtype,body}
+log({counter,Domain,Total}) ->
+	[Domain|_] = ?MYHOSTS, 
+	try
+		N = ejabberd_config:get_local_option({log_node,Domain}),
+		case net_adm:ping(N) of 
+			pang -> 
+				?INFO_MSG("write_log ::::> ~p",[{counter,Domain,Total}]);
+			pong ->
+				{logbox,N}!{counter,Domain,Total}
+		end 
+	catch
+		E:I ->
+			Err = erlang:get_stacktrace(),
+			?ERROR_MSG("write_log_error ::::> E=~p ; I=~p~n Error=~p",[E,I,Err]),
+			{error,E,I}
+	end;
 log(Packet) ->
 	[Domain|_] = ?MYHOSTS, 
 	try
